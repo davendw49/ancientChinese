36ce208c69fe97dd2258aa735636d6d82c288ca9
davendw49 <747031332@qq.com>
2018-09-29 04:38:00 +0800

    ubuntu

--- parse.py
+++ parse.py
@@ -1,63 +1,44 @@
 # -*- coding: utf-8 -*-
-from sgmllib import SGMLParser
+from bs4 import BeautifulSoup
+import re
+import codecs
 
-class GetIdList(SGMLParser):
-    def reset(self):
-        self.IDlist = []
-        self.flag = False
-        self.getdata = False
-        self.verbatim = 0
-        SGMLParser.reset(self)
-        
-    def start_div(self, attrs):
-        if self.flag == True:
-            self.verbatim +=1 #进入子层div了，层数加1
-            return
-        for k,v in attrs:#遍历div的所有属性以及其值
-            if k == 'src':
-                self.flag = True
-                return
- 
-    def end_div(self):#遇到</div>
-        if self.verbatim == 0:
-            self.flag = False
-        if self.flag == True:#退出子层div了，层数减1
-            self.verbatim -=1
- 
-    def start_p(self, attrs):
-        if self.flag == False:
-            return
-        self.getdata = True
-        
-    def end_p(self):#遇到</p>
-        if self.getdata:
-            self.getdata = False
- 
-    def handle_data(self, text):#处理文本
-        if self.getdata:
-            self.IDlist.append(text)
-            
-    def printID(self):
-        for i in self.IDlist:
-            print i
- 
+soup = BeautifulSoup(open('/home/daven/Github/ancientChinese/test.html'), features="html.parser")
+soup.prettify('utf-8')
 
-f = open('/Users/daven/Github/ancientChinese/test.html', 'r')
-##import urllib2
-##import datetime
-##vrg = (datetime.date(2012,2,19) - datetime.date.today()).days
-##strUrl = 'http://www.nod32id.org/nod32id/%d.html'%(200+vrg)
-##req = urllib2.Request(strUrl)#通过网络获取网页
-##response = urllib2.urlopen(req)
-##the_page = response.read()
-try:    
-    with f:        
-        the_page = f.read()
-except Exception as e:
-    print e
-    print the_page.split()
+file = codecs.open("/home/daven/Github/ancientChinese/testsoup.html", "w")
 
-lister = GetIdList()
-lister.feed(the_page)
-lister.printID()
-f.close()
+print >> file, soup
+
+file = codecs.open("/home/daven/Github/ancientChinese/testsoup.html", "r", "utf-8")
+
+collection = []
+sum=0
+sum1=0
+for line in file:
+	tmp = re.split(r'\s+', line.encode('utf-8'))
+	# print tmp
+	if len(tmp)>2:
+		
+		if tmp[0]=='':
+			sum+=1
+			tmp.pop(0)
+		else:
+			print tmp[0]
+
+
+		if tmp[len(tmp)-1]=='':
+			sum1+=1
+			tmp.pop(len(tmp)-1)
+		else:
+			print tmp[len(tmp)-1]
+
+		collection.append(tmp)
+	# print line
+
+
+# print len(collection)
+# print sum
+# print sum1
+for l in collection:
+	print l
